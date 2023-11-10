2ba35177fd5f0ae81a7cac9bd6bf9e33841d3050
Jay-Ubisse <joaquimubisse@gmail.com>
2023-11-10 14:28:49 +0200

    update "mitos e lendas" project

---
+++ mitos-lendas-africanas/js/script.js
@@ -0,0 +1,32 @@
+document.getElementById('titulo-mito-1').addEventListener('click', function() {
+    document.getElementById('titulo-mito-1').style.display = 'none';
+    document.getElementById('detalhes-mito-1').style.display = 'block';
+})
+
+document.getElementById('voltar-mito-1').addEventListener('click', function() {
+    document.getElementById('titulo-mito-1').style.display = 'flex';
+    document.getElementById('detalhes-mito-1').style.display = 'none';
+})
+
+///
+document.getElementById('titulo-mito-2').addEventListener('click', function() {
+    document.getElementById('titulo-mito-2').style.display = 'none';
+    document.getElementById('detalhes-mito-2').style.display = 'block';
+})
+
+document.getElementById('voltar-mito-2').addEventListener('click', function() {
+    document.getElementById('titulo-mito-2').style.display = 'flex';
+    document.getElementById('detalhes-mito-2').style.display = 'none';
+})
+
+///
+
+document.getElementById('titulo-mito-3').addEventListener('click', function() {
+    document.getElementById('titulo-mito-3').style.display = 'none';
+    document.getElementById('detalhes-mito-3').style.display = 'block';
+})
+
+document.getElementById('voltar-mito-3').addEventListener('click', function() {
+    document.getElementById('titulo-mito-3').style.display = 'flex';
+    document.getElementById('detalhes-mito-3').style.display = 'none';
+})
